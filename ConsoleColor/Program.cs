be62057e5adff48105bbb6953b9b717f8d3c8a4e
EdgarIvanLorenzo <edgariva80@gmail.com>
2021-10-15 12:59:48 -0500

    Dos

---
+++ ConsoleColor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //En esta clase vamos a ver como poner el texto y fondo de un color en c#
+            Console.BackgroundColor = System.ConsoleColor.Cyan; //Cambiamos el color de fondo de laterminal
+            Console.ForegroundColor = System.ConsoleColor.Red; //Cambiamoe el color a las letras
+            Console.WriteLine("Hola");
+        }
+    }
+}
---
+++ Leer_Linea/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Leer_Linea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Todo lo que se lea mediante la consola es de tipo string u String
+            
+            //Dato de tipo entero
+            Console.WriteLine("Ingrese un numero"); //Mandamos un mensaje en la consola
+            int dato = Convert.ToInt32(Console.ReadLine()); //Leemos un dato de tipo int32 Bytes para ello se realiza una convercion
+            Console.WriteLine("El dato es de: "+ dato); //Se imprime el valor ingresado
+            
+            //Dato de tipo Cadena u string leer un todo el caracter
+            Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine(); //Leemos un dato de tipo cadena sin convertir a un tipo numerico
+            Console.WriteLine("El nombre de la persona es: "+ nombre);
+            
+            //Leemos un dato de tipo char
+            Console.WriteLine("Ingrese su M F");
+            char sexo = Convert.ToChar(Console.Read()); //Leemos un dato de tipo cadena sin convertir a un tipo numerico
+            Console.WriteLine("El nombre de la persona es: "+ sexo);
+
+            
+        }
+    }
+}
